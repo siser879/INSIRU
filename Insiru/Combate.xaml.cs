cd99bc3ca98388ed21268be16ad9ed29f600e4b0
siser 879 <siser879@gmail.com>
2023-05-02 17:05:10 +0200

    Refactorización Combate.xaml.cs

--- Insiru/Combate.xaml.cs
+++ Insiru/Combate.xaml.cs
@@ -1,23 +1,16 @@
 ﻿using System;
 using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace Insiru
 {
     /// <summary>
     /// Lógica de interacción para Combate.xaml
     /// </summary>
+
     public partial class Combate : Window
     {
 
@@ -110,5 +103,6 @@ namespace Insiru
             }
             
         }
+
     }
 }
