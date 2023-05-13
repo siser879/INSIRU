61deede1316053296aedd8e49edffe3e8f046959
Rafael Sánchez Fernández <129056124+p-rsanchez@users.noreply.github.com>
2023-05-13 18:53:11 +0200

    Commit

--- Insiru/Combate.xaml.cs
+++ Insiru/Combate.xaml.cs
@@ -112,70 +112,269 @@ namespace Insiru
         }
 
         //Turnos
-        private void Turno()
+        private int elegir_ataque_enemigo()
         {
-            //if (turnoJugador == true)
-            //{
-            //    MessageBox.Show("Es turno del jugador");
-            //    turnoJugador = false;
-            //}
-            //else {
-            //    MessageBox.Show("Es turno de la maquina");
-
-
-            //    turnoJugador = true;
-            //}
-
-            MessageBox.Show("Es turno de la maquina");
-
             Random rnd = new Random();
 
+            string nombre_ataque = "";
+            if (pokemon_enemigo.Tipo == "Fuego")
+            {
+                nombre_ataque = "Lanzallamas";
+            }
+            else if (pokemon_enemigo.Tipo == "Agua")
+            {
+                nombre_ataque = "Pistola Agua";
+            }
+            else if (pokemon_enemigo.Tipo == "Planta")
+            {
+                nombre_ataque = "Hoja Afilada";
+            };
+
             int eleccion = rnd.Next(1, 5);
+            eleccion = 4;
+            return eleccion;
+        }
 
-            if (eleccion == 1) { Ataque1_Metodo(Vida_Aliado, pokemon_aliado, pokemon_aliado_maxVida, false); }
-            else if (eleccion == 2) { Ataque2_Metodo(Vida_Enemigo, pokemon_enemigo, pokemon_enemigo_maxVida, false); }
-            else if (eleccion == 3) { Ataque3_Metodo(Vida_Enemigo, pokemon_enemigo, pokemon_enemigo_maxVida, false); }
-            else if (eleccion == 4) { Ataque4_Metodo(Vida_Aliado, pokemon_aliado, pokemon_enemigo, pokemon_aliado_maxVida, false); }
+        private void mensaje_ataque_enemigo(int eleccion)
+        {
+            string nombre_ataque = "";
 
-            MessageBox.Show("Es turno del jugador");
+            if (pokemon_enemigo.Tipo == "Fuego")
+            {
+                nombre_ataque = "Lanzallamas";
+            }
+            else if (pokemon_enemigo.Tipo == "Agua")
+            {
+                nombre_ataque = "Pistola Agua";
+            }
+            else if (pokemon_enemigo.Tipo == "Planta")
+            {
+                nombre_ataque = "Hoja Afilada";
+            };
 
+            if (eleccion == 1) { MessageBox.Show(pokemon_enemigo.Nombre + " uso Placaje"); }
+            else if (eleccion == 2) { MessageBox.Show(pokemon_enemigo.Nombre + " uso Esquivar"); }
+            else if (eleccion == 3) { MessageBox.Show(pokemon_enemigo.Nombre + " uso Curar"); }
+            else { MessageBox.Show(pokemon_enemigo.Nombre + " uso " + nombre_ataque); }
+        }
+
+        private Boolean defensa()
+        {
+            Random random = new Random();
+
+            if (random.Next(0, 10) >= 5){ return true; }
+            else{ return false; }
         }
 
         //Placaje
         private void Ataque1_Click(object sender, RoutedEventArgs e)
         {
-            Ataque1_Metodo(Vida_Enemigo, pokemon_enemigo, pokemon_enemigo_maxVida, true);
+            MessageBox.Show(pokemon_aliado.Nombre + " uso Placaje");
+
+            int ataque_enemigo = elegir_ataque_enemigo();
+
+            switch (ataque_enemigo)
+            {
+                case 1:
+                    //ALIADO BAJA VIDA ENEMIGO
+                    Ataque1_Metodo(Vida_Enemigo, pokemon_enemigo, pokemon_enemigo_maxVida);
+                    //ENEMIGO BAJA VIDA ALIADO
+                    mensaje_ataque_enemigo(ataque_enemigo);
+                    Ataque1_Metodo(Vida_Aliado, pokemon_aliado, pokemon_aliado_maxVida);
+                    break;
+
+                case 2:
+                    //DEFENSA DEL ENEMIGO
+                    mensaje_ataque_enemigo(ataque_enemigo);
+                    if (!defensa())
+                    {
+                        //ALIADO BAJA VIDA ENEMIGO
+                        MessageBox.Show(pokemon_enemigo.Nombre + " fallo al esquivar!");
+                        Ataque1_Metodo(Vida_Enemigo, pokemon_enemigo, pokemon_enemigo_maxVida);
+                    }
+                    else
+                    {
+                        //ENEMIGO ESQUIVA CORRECTAMENTE
+                        MessageBox.Show("Ataque esquivado por " + pokemon_enemigo.Nombre + "!");
+                    }
+                    break;
+
+                case 3:
+                    //ALIADO BAJA VIDA ENEMIGO
+                    Ataque1_Metodo(Vida_Enemigo, pokemon_enemigo, pokemon_enemigo_maxVida);
+                    //ENEMIGO SE CURA
+                    mensaje_ataque_enemigo(ataque_enemigo);
+                    Ataque3_Metodo(Vida_Enemigo, pokemon_enemigo, pokemon_aliado, pokemon_enemigo_maxVida, false);
+                    break;
+
+                case 4:
+                    //ALIADO BAJA VIDA ENEMIGO
+                    Ataque1_Metodo(Vida_Enemigo, pokemon_enemigo, pokemon_enemigo_maxVida);
+                    //ENEMIGO ELEMENTAL A ALIADO
+                    mensaje_ataque_enemigo(ataque_enemigo);
+                    Ataque4_Metodo(Vida_Aliado, pokemon_aliado, pokemon_enemigo, pokemon_aliado_maxVida);
+                    break;
+            }
         }
 
         //Esquivar
         private void Ataque2_Click(object sender, RoutedEventArgs e)
         {
-            Ataque2_Metodo(Vida_Aliado, pokemon_aliado, pokemon_aliado_maxVida, true);
+            MessageBox.Show(pokemon_aliado.Nombre + " uso Esquivar");
+
+            int ataque_enemigo = elegir_ataque_enemigo();
+
+            switch (ataque_enemigo)
+            {
+                case 1:
+                    //DEFENSA ALIADO
+                    mensaje_ataque_enemigo(ataque_enemigo);
+                    if (!defensa())
+                    {
+                        //ENEMIGO BAJA VIDA ALIADO
+                        MessageBox.Show(pokemon_aliado.Nombre + " fallo al esquivar!");
+                        Ataque1_Metodo(Vida_Aliado, pokemon_aliado, pokemon_aliado_maxVida);
+                    }
+                    else
+                    {
+                        //ALIADO ESQUIVA CORRECTAMENTE
+                        MessageBox.Show("Ataque esquivado por " + pokemon_enemigo.Nombre + "!");
+                    }
+                    break;
+
+                case 2:
+                    mensaje_ataque_enemigo(ataque_enemigo);
+                    MessageBox.Show(pokemon_aliado.Nombre + " fallo al esquivar!");
+                    MessageBox.Show(pokemon_enemigo.Nombre + " fallo al esquivar!");
+                    break;
+
+                case 3:
+                    //ENEMIGO SE CURA
+                    mensaje_ataque_enemigo(ataque_enemigo);
+                    Ataque3_Metodo(Vida_Enemigo, pokemon_enemigo, pokemon_aliado, pokemon_enemigo_maxVida, false);
+                    break;
+
+                case 4:
+                    //DEFENSA ALIADO
+                    mensaje_ataque_enemigo(ataque_enemigo);
+                    if (!defensa())
+                    {
+                        //ENEMIGO BAJA VIDA ALIADO
+                        MessageBox.Show(pokemon_aliado.Nombre + " fallo al esquivar!");
+                        Ataque4_Metodo(Vida_Aliado, pokemon_aliado, pokemon_enemigo, pokemon_aliado_maxVida);
+                    }
+                    else
+                    {
+                        //ALIADO ESQUIVA CORRECTAMENTE
+                        MessageBox.Show("Ataque esquivado por " + pokemon_enemigo.Nombre + "!");
+                    }
+                    break;
+            }
         }
 
         //Curar
         private void Ataque3_Click(object sender, RoutedEventArgs e)
         {
-            Ataque3_Metodo(Vida_Aliado, pokemon_aliado, pokemon_aliado_maxVida, true);
+            MessageBox.Show(pokemon_aliado.Nombre + " uso Curar");
+
+            int ataque_enemigo = elegir_ataque_enemigo();
+
+            switch (ataque_enemigo)
+            {
+                case 1:
+                    //ALIADO SE CURA
+                    Ataque3_Metodo(Vida_Aliado, pokemon_aliado, pokemon_enemigo, pokemon_aliado_maxVida, true);
+                    //ENEMIGO BAJA VIDA ALIADO
+                    mensaje_ataque_enemigo(ataque_enemigo);
+                    Ataque1_Metodo(Vida_Aliado, pokemon_aliado, pokemon_aliado_maxVida);
+                    break;
+
+                case 2:
+                    mensaje_ataque_enemigo(ataque_enemigo);
+                    MessageBox.Show(pokemon_enemigo.Nombre + " fallo al esquivar!");
+                    break;
+
+                case 3:
+                    //ALIADO SE CURA
+                    Ataque3_Metodo(Vida_Aliado, pokemon_aliado, pokemon_enemigo, pokemon_aliado_maxVida, true);
+                    //ENEMIGO SE CURA
+                    mensaje_ataque_enemigo(ataque_enemigo);
+                    Ataque3_Metodo(Vida_Enemigo, pokemon_enemigo, pokemon_aliado, pokemon_enemigo_maxVida, false);
+                    break;
+
+                case 4:
+                    //ALIADO SE CURA
+                    Ataque3_Metodo(Vida_Aliado, pokemon_aliado, pokemon_enemigo, pokemon_aliado_maxVida, true);
+                    //ENEMIGO ELEMENTAL A ALIADO
+                    mensaje_ataque_enemigo(ataque_enemigo);
+                    Ataque4_Metodo(Vida_Aliado, pokemon_aliado, pokemon_enemigo, pokemon_aliado_maxVida);
+                    break;
+
+            }
         }
 
         //Elemental
         private void Ataque4_Click(object sender, RoutedEventArgs e)
         {
-            Ataque4_Metodo(Vida_Enemigo, pokemon_enemigo, pokemon_aliado, pokemon_enemigo_maxVida, true);
+            MessageBox.Show(pokemon_aliado.Nombre + " uso " + Ataque4.Content);
+
+            int ataque_enemigo = elegir_ataque_enemigo();
+
+            switch (ataque_enemigo)
+            {
+                case 1:
+                    //ALIADO ELEMENTAL A ENEMIGO
+                    Ataque4_Metodo(Vida_Enemigo, pokemon_enemigo, pokemon_aliado, pokemon_enemigo_maxVida);
+                    //ENEMIGO BAJA VIDA ALIADO
+                    mensaje_ataque_enemigo(ataque_enemigo);
+                    Ataque1_Metodo(Vida_Aliado, pokemon_aliado, pokemon_aliado_maxVida);
+                    break;
+
+                case 2:
+                    //DEFENSA DEL ENEMIGO
+                    mensaje_ataque_enemigo(ataque_enemigo);
+                    if (!defensa())
+                    {
+                        //ALIADO BAJA VIDA ENEMIGO
+                        MessageBox.Show(pokemon_enemigo.Nombre + " fallo al esquivar!");
+                        Ataque4_Metodo(Vida_Enemigo, pokemon_enemigo, pokemon_aliado, pokemon_enemigo_maxVida);
+                    }
+                    else
+                    {
+                        //ENEMIGO ESQUIVA CORRECTAMENTE
+                        MessageBox.Show("Ataque esquivado por " + pokemon_enemigo.Nombre + "!");
+                    }
+                    break;
+
+                case 3:
+                    //ALIADO ELEMENTAL A ENEMIGO
+                    Ataque4_Metodo(Vida_Enemigo, pokemon_enemigo, pokemon_aliado, pokemon_enemigo_maxVida);
+                    //ENEMIGO SE CURA
+                    mensaje_ataque_enemigo(ataque_enemigo);
+                    Ataque3_Metodo(Vida_Enemigo, pokemon_enemigo, pokemon_aliado, pokemon_enemigo_maxVida, false);
+                    break;
+
+                case 4:
+                    //ALIADO ELEMENTAL A ENEMIGO
+                    Ataque4_Metodo(Vida_Enemigo, pokemon_enemigo, pokemon_aliado, pokemon_enemigo_maxVida);
+                    //ENEMIGO ELEMENTAL A ALIADO
+                    mensaje_ataque_enemigo(ataque_enemigo);
+                    Ataque4_Metodo(Vida_Aliado, pokemon_aliado, pokemon_enemigo, pokemon_aliado_maxVida);
+                    break;
+            }
         }
 
-        private double Danio_Elemental(string tipo)
+        private double Danio_Elemental(string tipo_aliado, string tipo_enemigo)
         {
             double danio_por_tipo = 5;
 
-            if (pokemon_aliado.Tipo == "Fuego")
+            if (tipo_aliado == "Fuego")
             {
-                if (pokemon_enemigo.Tipo == "Fuego")
+                if (tipo_enemigo == "Fuego")
                 {
                     return danio_por_tipo;
                 }
-                else if (pokemon_enemigo.Tipo == "Agua")
+                else if (tipo_enemigo == "Agua")
                 {
                     return danio_por_tipo *= 0.5;
                 }
@@ -184,13 +383,13 @@ namespace Insiru
                     return danio_por_tipo *= 2;
                 }
             }
-            else if (pokemon_aliado.Tipo == "Agua")
+            else if (tipo_aliado == "Agua")
             {
-                if (pokemon_enemigo.Tipo == "Fuego")
+                if (tipo_enemigo == "Fuego")
                 {
                     return danio_por_tipo *= 2;
                 }
-                else if (pokemon_enemigo.Tipo == "Agua")
+                else if (tipo_enemigo == "Agua")
                 {
                     return danio_por_tipo;
                 }
@@ -201,11 +400,11 @@ namespace Insiru
             }
             else
             {
-                if (pokemon_enemigo.Tipo == "Fuego")
+                if (tipo_enemigo == "Fuego")
                 {
                     return danio_por_tipo *= 0.5;
                 }
-                else if (pokemon_enemigo.Tipo == "Agua")
+                else if (tipo_enemigo == "Agua")
                 {
                     return danio_por_tipo *= 2;
                 }
@@ -216,8 +415,9 @@ namespace Insiru
             }
         }
 
-        private void Ataque1_Metodo(Rectangle campo, Pokemon pokemon, int vidaMax, Boolean turnoMaquina)
+        private void Ataque1_Metodo(Rectangle campo, Pokemon pokemon, int vidaMax)
         {
+
             double widthBarra = ((pokemon.Vida - 5) * 164) / vidaMax;
 
             if (widthBarra <= 0)
@@ -225,74 +425,52 @@ namespace Insiru
                 campo.Width = 0;
                 pokemon.Vida = 0;
 
-                //Enviar a la pantalla de victoria
+                //Enviar a la pantalla de victoria o derrota
 
             }
             else
             {
                 campo.Width = widthBarra;
                 pokemon.Vida -= 5;
-                if (turnoMaquina == true)
-                {
-                    Turno();
-                }
             }
         }
 
-        private void Ataque2_Metodo(Rectangle campo, Pokemon pokemon, int vidaMax, Boolean turnoMaquina)
-        {
-            double widthBarra = ((pokemon.Vida - 5) * 164) / vidaMax;
-
-            Random random = new Random();
-            int numeroAleatorio = random.Next(0, 10);
-
-            if (numeroAleatorio >= 0)
-            {
-                MessageBox.Show("Ataque esquivado");
-            }
-            else
-            {
-
-                if (turnoMaquina == true)
-                {
-                    Turno();
-                }
-            }
-        }
-
-        private void Ataque3_Metodo(Rectangle campo, Pokemon pokemon, int vidaMax, Boolean turnoMaquina)
+        private void Ataque3_Metodo(Rectangle campo, Pokemon pokemon, Pokemon enemigo, int vidaMax, Boolean aliado)
         {
             double widthBarra = ((pokemon.Vida + 3) * 164) / vidaMax;
 
-            if (widthBarra <= 0)
+            if(pokemon.Vida == vidaMax)
             {
-                campo.Width = 0;
-                pokemon.Vida = 0;
-
-                //Enviar a la pantalla de derrota
-
-            }
-            else if (widthBarra >= 164)
-            {
-
-                //Mostrar mensaje - Ya tienes la vida al máximo
-                MessageBox.Show("Ya tienes la vida al máximo");
-
-            }
-            else
-            {
-                campo.Width = widthBarra;
-                pokemon.Vida += 3;
-                if (turnoMaquina == true)
+                if (aliado)
                 {
-                    Turno();
+                    //Mostrar mensaje - Ya tienes la vida al máximo
+                    MessageBox.Show(pokemon.Nombre + " se intentado curar pero no tuvo efecto.");
+                }
+                else
+                {
+                    //Mostrar mensaje - Ya tienes la vida al máximo
+                    MessageBox.Show(enemigo.Nombre + " se intento curar pero no tuvo efecto.");
+                }
+                
+            }else
+            {
+                if(pokemon.Vida + 3 > vidaMax)
+                {
+                    campo.Width = 164;
+                    pokemon.Vida = vidaMax;
+                }
+                else
+                {
+                    campo.Width = widthBarra;
+                    pokemon.Vida += 3;
                 }
             }
         }
 
-        private void Ataque4_Metodo(Rectangle campo, Pokemon pokemon1, Pokemon pokemon2, int vidaMax, Boolean turnoMaquina)
+        private void Ataque4_Metodo(Rectangle campo, Pokemon pokemon1, Pokemon pokemon2, int vidaMax)
         {
-            double widthBarra = ((pokemon1.Vida - Danio_Elemental(pokemon2.Tipo)) * 164) / vidaMax;
+
+            double widthBarra = ((pokemon1.Vida - Danio_Elemental(pokemon2.Tipo, pokemon1.Tipo)) * 164) / vidaMax;
 
             if (widthBarra <= 0)
             {
@@ -305,11 +483,7 @@ namespace Insiru
             else
             {
                 campo.Width = widthBarra;
-                pokemon1.Vida -= Convert.ToInt32(Danio_Elemental(pokemon2.Tipo));
-                if (turnoMaquina == true)
-                {
-                    Turno();
-                }
+                pokemon1.Vida -= Convert.ToInt32(Danio_Elemental(pokemon2.Tipo, pokemon1.Tipo));
             }
         }
 
@@ -318,5 +492,6 @@ namespace Insiru
             MessageBox.Show("Has abandonado la partida");
             Close();
         }
+
     }
 }
